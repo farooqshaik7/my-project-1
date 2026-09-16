283124764a5ebfcbc6e2c6273aa9483238a7160b
Farooq shaik <farooqshaik77777@gmail.com>
2026-09-16 23:50:00 +0530

    Added promises.js

---
+++ promises.js
@@ -0,0 +1,29 @@
+                 // (Promise class) PROMISES :
+                 //                        Promises class give you a promise, that I will return you something in the future. 
+
+                                            // defining a promise is hard
+                                            //using a promise is easy
+
+// A promise in Javascript is an object that represents the eventual completion (or failure) of an synchronous operation and its resulting value.
+
+
+function setTimeoutPromisified(ms) {        //ms = millisecond
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+function callback () {
+   console.log("5 seconds have passed");                     //Returns an object of an promise class.     
+}
+
+setTimeoutPromisified(5000).then(callback)
+
+//////////////////////////////////////////////////////
+
+function waitFor3S(resolve) {
+    setTimeout(resolve, 6000)
+}
+
+function main() {
+    console.log("main is called")
+}
+
+waitFor3S(main)
